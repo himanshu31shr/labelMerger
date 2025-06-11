ee98b5c566098b9db4f5c3a39b9b090d2f198da8
himanshu31shr <himanshu31shr@gmail.com>
2025-06-11 19:01:31 +0000

    Deploying to gh-pages from @ himanshu31shr/flipkart-amazon-tools@705804b387ebc4761c188936efcfab4502885327 🚀

--- assets/FormattedCurrency-CmX2WUI9.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./mui-vendor-oyUBTadH.js";const s=({value:r,color:t})=>{const e=new Intl.NumberFormat("en-US",{style:"currency",currency:"INR"});return n.jsx("span",{style:{color:t??"inherit"},children:e.format(r)})};export{s as F};
